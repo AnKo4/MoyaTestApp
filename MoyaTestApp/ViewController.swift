f3aadd76c603b05fb0ca13e1d127bc3c32333a6c
Andrey Kochetkov <akochetkov@65apps.com>
2019-09-30 17:58:18 +0400

    Initial Commit

---
+++ MoyaTestApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MoyaTestApp
+//
+//  Created by Andrey on 30/09/2019.
+//  Copyright © 2019 Andrey. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
